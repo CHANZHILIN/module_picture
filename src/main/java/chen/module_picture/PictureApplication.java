3197e9461b74aecc8208e22b4a2df778ce1bd01c
chanzhilin <1181785848@qq.com>
2019-05-14 16:12:13 +0800

    添加各种Application

---
+++ src/main/java/chen/module_picture/PictureApplication.java
@@ -0,0 +1,23 @@
+package chen.module_picture;
+
+import android.app.Application;
+
+import chen.baselib.BaseApplication;
+import chen.baselib.IComponentApplication;
+
+/**
+ * Introduce :
+ * Created by CHEN_ on 2019/5/14.
+ * PACKAGE_NAME : chen.module_picture
+ **/
+public class PictureApplication implements IComponentApplication {
+    @Override
+    public void onCreate(BaseApplication application) {
+
+    }
+
+    @Override
+    public Application getAppliaction() {
+        return null;
+    }
+}
